183e8662774d87fa70e8c193bc2e87d17f7c94ed
Amrit Bhatttacharjee <ab181094@gmail.com>
2016-10-19 20:42:38 +0600

    Works fine with kitkat. Next target is to add marker listener

--- app/src/main/java/com/example/amrit/cmchguide/MainActivity.java
+++ app/src/main/java/com/example/amrit/cmchguide/MainActivity.java
@@ -1,13 +1,77 @@
 package com.example.amrit.cmchguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.amrit.cmchguide.Map.MapActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] buildings;
+    String[] xArray;
+    String[] yArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buildings = getResources().getStringArray(R.array.buildingsArray);
+        xArray = getResources().getStringArray(R.array.xArray);
+        yArray = getResources().getStringArray(R.array.yArray);
+    }
+
+    public void gotoMain(View view) {
+        Intent intent = new Intent(MainActivity.this, MapActivity.class);
+        intent.putExtra("name", buildings[0]);
+        intent.putExtra("x", xArray[0]);
+        intent.putExtra("y", yArray[0]);
+        startActivity(intent);
+    }
+
+    public void gotoNew(View view) {
+        Intent intent = new Intent(MainActivity.this, MapActivity.class);
+        intent.putExtra("name", buildings[1]);
+        intent.putExtra("x", xArray[1]);
+        intent.putExtra("y", yArray[1]);
+        startActivity(intent);
+    }
+
+    public void gotoCafe(View view) {
+        Intent intent = new Intent(MainActivity.this, MapActivity.class);
+        intent.putExtra("name", buildings[2]);
+        intent.putExtra("x", xArray[2]);
+        intent.putExtra("y", yArray[2]);
+        startActivity(intent);
+    }
+
+    public void gotoMedical(View view) {
+        Intent intent = new Intent(MainActivity.this, MapActivity.class);
+        intent.putExtra("name", buildings[3]);
+        intent.putExtra("x", xArray[3]);
+        intent.putExtra("y", yArray[3]);
+        startActivity(intent);
+    }
+
+    public void gotoNursing(View view) {
+        Intent intent = new Intent(MainActivity.this, MapActivity.class);
+        intent.putExtra("name", buildings[4]);
+        intent.putExtra("x", xArray[4]);
+        intent.putExtra("y", yArray[4]);startActivity(intent);
+    }
+
+    public void gotoDental(View view) {
+        Intent intent = new Intent(MainActivity.this, MapActivity.class);
+        intent.putExtra("name", buildings[5]);
+        intent.putExtra("x", xArray[5]);
+        intent.putExtra("y", yArray[5]);
+        startActivity(intent);
+    }
+
+    private void toastIt(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/amrit/cmchguide/map/MapActivity.java
@@ -0,0 +1,78 @@
+package com.example.amrit.cmchguide.Map;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.amrit.cmchguide.R;
+import com.qozix.tileview.TileView;
+
+public class MapActivity extends AppCompatActivity {
+
+    LinearLayout linearLayout;
+    TileView tileView;
+    ImageView marker;
+    String building = null;
+    int x = 0, y = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        if(bundle != null) {
+            building = bundle.getString("name");
+            x = Integer.parseInt(bundle.getString("x"));
+            y = Integer.parseInt(bundle.getString("y"));
+            //toastIt(buiding + ": " + x + ", " + y);
+        }
+
+        linearLayout = (LinearLayout) findViewById(R.id.tileLayout);
+
+        tileView = new TileView(this);
+
+        //create tileview -- Amrit
+        tileView.setSize(3000, 2000);
+
+        //Setting the minimum parameters from the documentation -- Amrit
+        tileView.addDetailLevel(1f, "tiles/map/1000/%d_%d.gif", "samples/map.jpg");
+        tileView.addDetailLevel(0.5f, "tiles/map/500/%d_%d.gif", "samples/map.jpg");
+        tileView.addDetailLevel(0.25f, "tiles/map/250/%d_%d.gif", "samples/map.jpg");
+        tileView.addDetailLevel(0.125f, "tiles/map/125/%d_%d.gif", "samples/map.jpg");
+
+        tileView.moveToAndCenter(3000, 2000);
+        tileView.slideToAndCenter(3000, 2000);
+        tileView.setScale(0.25);
+
+        if(building != null) {
+            marker = new ImageView(this);
+            marker.setImageResource(R.drawable.marker);
+            marker.setTag(building);
+
+            tileView.addMarker(marker, x, y, -0.5f, -1.0f);
+            tileView.setScale(1f);
+            tileView.slideToAndCenter(x-200, y-200);
+        }
+
+        linearLayout.addView(tileView);
+
+        /*tileView.addTileViewEventListener(new TileView.TileViewEventListenerImplementation(){
+            @Override
+            public void onTap(int x, int y) {
+                toastIt("The co-ordinate of this place is: " + x + ", " +y);
+            }
+        });*/
+    }
+
+    private void toastIt(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+    }
+}
